f57d780d8d9ce74adbaf1bbe9065e92ee310ebe5
Sasha <Sashastepanyan4@gmail.com>
2018-02-26 16:44:02 +0400

    fix error

--- app/models/category.js
+++ app/models/category.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('answers', {
+module.exports = mongoose.model('category', {
     name : {type : String, default: 'php'},
 
 });
