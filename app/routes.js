49c6ff8a5ddc04c41b0746fd53e3fddddd9070cd
Alina1 <alinakirakosyan1994@mail.ru>
2018-03-06 22:34:51 +0300

    some massage

--- app/routes.js
+++ app/routes.js
@@ -2,26 +2,91 @@
 
 // grab the nerd model we just created
 
-
-var employee_model = require('./models/employee');
+var Category = require('./models/category');
+var Answers = require('./models/answer');
 module.exports = function(app) {
 
     // server routes ===========================================================
     // handle things like api calls
     // authentication routes
-    app.get('/api/employee', function(req, res) {
 
-        employee_model.find(function(err, data) {
-                       if (err)
-                               res.send(err);
-            console.log(data);
-                           res.json(data); // return all nerds in JSON format
-                });
-
-    });
+    // sample api route
 
 
-
+//     app.get('/api/category', function(req, res) {
+//
+//         Answers.find(function(err, data) {
+//
+//             // if there is an error retrieving, send the error.
+//             // nothing after res.send(err) will execute
+//             if (err)
+//                 res.send(err);
+//
+//             res.json(data); // return all nerds in JSON format
+//         });
+//
+//     });
+//     app.get('/api/category/{{id}}', function(req, res) {
+//         db.get().collection("someCollection").find().toArray(function (err, docs) {
+//             if(err){
+//                 console.log(err);
+//                 return res.sendStatus(500);
+//             }
+//             res.send(docs);
+//
+//         })
+// //get info for specific {{id}}
+//
+//     });
+//     app.post('/api/category', function(req, res) {
+//         var employee = {
+//             name: req.body.name
+//         };
+//         db.get().collection("nameCollection").insert(nameCollection, function () {
+//             if (err) {
+//                 console.log(err);
+//                 return res.sendStatus(500);
+//             }
+//             res.send(docs);
+//
+//         })
+//     //get infor from request and store in to db
+//
+//     });
+//     app.put('/api/category/{{id}}', function(req, res) {
+//         db.get().collection('nameCollection').updateOn(
+//             {_id:ObjectID(req.params.id)},
+//             {name:req.body.name},
+//             function (err, result) {
+//                 if (err){
+//                     console.log(err);
+//                     return res.sendStatus(200);
+//                 }
+//             }
+//         )
+//
+//        //upate info from request in post for data with {{id}}
+//
+//     });
+//     app.delete('/api/category/{{id}}', function(req, res) {
+//         db.get().collection("nameCollection").deleteOne(
+//             {_id: ObjectID(req.params.id)},
+//             function (err, result) {
+//                 if(err){
+//                     console.log(err);
+//                     return res.sendStatus(500);
+//                 }
+//                 res.sendStatus(200);
+//             }
+//         )
+//
+//         //delete info for {{id}}
+//
+//     });
+//
+//     exports.get = function () {
+//         return state.db;
+//     };
     // route to handle creating goes here (app.post)
     // route to handle delete goes here (app.delete)
 
@@ -32,3 +97,4 @@ module.exports = function(app) {
     });
 
 };
+
--- public/js/controllers/EmployeeCtrl.js
+++ public/js/controllers/EmployeeCtrl.js
@@ -1,18 +1,29 @@
 angular.module('EmployeeCtrl', []).controller('EmployeeController', function($scope, $http,$interval) {
     $scope.title = "Employee List 2";
     $scope.description = "test Description";
-    $scope.list = [];
+    $scope.list = [
+        {
+            firstName : "Ani",
+            lastName : "Karapetyan",
+            email : "Karapetyan@gmail.com"
+        },
+        {
+            firstName : "Anahit",
+            lastName : "Karapetyan1",
+            email : "Karapetyan1@gmail.com"
+        }
+    ]
 
-    $http({
-        method: 'GET',
-        url: '/api/employee'
-    }).then(function successCallback(response) {
-        console.log(response);
-        $scope.list = response.data;
-
-    }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-    });
+    // $http({
+    //     method: 'GET',
+    //     url: '/api/employee'
+    // }).then(function successCallback(response) {
+    //     console.log(response);
+    //     $scope.list = response.data;
+    //
+    // }, function errorCallback(response) {
+    //     // called asynchronously if an error occurs
+    //     // or server returns response with an error status.
+    // });
 
 });
