6e4d46f2ddbfb10eaeae7a919cd8cc06f6c5c578
Larisa Melqonyan <larisa.melqonyan94@gmail.com>
2018-03-29 01:10:37 +0300

    Connect to CSV file

--- public/js/controllers/StudentCtrl.js
+++ public/js/controllers/StudentCtrl.js
@@ -1,18 +1,27 @@
-angular.module('StudentCtrl', []).controller('StudentController', function($scope) {
+angular.module('StudentCtrl', []).controller('StudentController', function($scope, $http) {
     $scope.title = "Student List 2";
     $scope.description = "test Description for student";
     $scope.list = [];
 
-    $scope.list = [
-        {
-            FirstName:"Poxos",
-            LastName:"Student",
-            Email:"Poxosyan@gmail.com",
-        },
-        {
-            FirstName:"Petros",
-            LastName:"Student",
-            Email:"petrosyan@gmail.com"
-        }
-    ];
+   $scope.readCSV = function() {
+          // http get request to read CSV file content
+          $http.get('SalesJan2009.csv').then(function(response){
+              var allTextLines = response.data.split(/\r\n|\n/);
+              var headers = allTextLines[0].split(',');
+              var lines = [];
+
+              for ( var i = 0; i < allTextLines.length; i++) {
+                  // split content based on comma
+                  var data = allTextLines[i].split(',');
+                  if (data.length == headers.length) {
+                      var tarr = [];
+                      for ( var j = 0; j < headers.length; j++) {
+                          tarr.push(data[j]);
+                      }
+                      lines.push(tarr);
+                  }
+              }
+              $scope.data = lines;
+          })
+      };
 });
